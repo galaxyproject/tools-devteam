84e3497f464048c6c9b0494205d6d0b7c8082e8d
Dave B <dave@bx.psu.edu>
2015-04-20 11:09:10 -0400

    Update kraken-report.

--- tool_collections/kraken/kraken-report/planemo_commands/planemo_command_kraken.sh
+++
@@ -1,7 +0,0 @@
-planemo tool_init --force --id "kraken" --name "Classify with kraken" \
-			--requirement Kraken@0.10.3-beta \
-			--example_command 'kraken --db $DBNAME seqs.fa > classified_seqs.txt' \
-			--example_input seqs.fa  \
-			--example_output classified_seqs.txt \
-			--test_case \
-			--help_from_command 'kraken --help'
