f7d1a715b4f792982c1115d05c448378ddb3c613
Dave B <dave@bx.psu.edu>
2015-08-06 14:04:23 -0400

    Add data manager to fetch NCBI taxonomy.

---
+++ data_managers/data_manager_fetch_ncbi_taxonomy/data_manager/data_manager.py
@@ -0,0 +1,28 @@
+import json
+import argparse
+import os
+import shutil
+
+parser = argparse.ArgumentParser(description='Create data manager json.')
+parser.add_argument('--out', dest='output', action='store', help='JSON filename')
+parser.add_argument('--name', dest='name', action='store', help='Data table entry unique ID')
+
+args = parser.parse_args()
+
+def main(args):
+    data_manager_entry = {}
+    data_manager_entry['value'] = args.name.lower()
+    data_manager_entry['name'] = args.name
+    data_manager_entry['path'] = '.'
+    data_manager_json = dict(data_tables=dict(ncbi_taxonomy=data_manager_entry))
+    params = json.loads(open(args.output).read())
+    target_directory = params['output_data'][0]['extra_files_path']
+    os.mkdir(target_directory)
+    output_path = os.path.abspath(os.getcwd())
+    for filename in [ 'names.dmp', 'nodes.dmp' ]:
+        shutil.move(os.path.join(output_path, filename), target_directory)
+    file(args.output, 'w').write(json.dumps(data_manager_json))
+
+if __name__ == '__main__':
+    main(args)
+
