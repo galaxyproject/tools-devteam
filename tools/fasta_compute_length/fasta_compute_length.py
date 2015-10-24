f3abe716790328debc9ab58b8b036ee29af09d7d
Eric Rasche <rasche.eric@gmail.com>
2015-10-24 18:31:52 -0500

    Support keep_first_word option

--- tools/fasta_compute_length/fasta_compute_length.py
+++ tools/fasta_compute_length/fasta_compute_length.py
@@ -6,4 +6,4 @@ Uses fasta_to_len converter code.
 import sys
 from utils.fasta_to_len import compute_fasta_length
 
-compute_fasta_length( sys.argv[1], sys.argv[2], sys.argv[3], False )
+compute_fasta_length( sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4] == 'id_only' )
