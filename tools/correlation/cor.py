d1d8fb2fd6a30a0e7cb0bb543c4a2883978e54d7
Bjoern Gruening <bjoern.gruening@gmail.com>
2018-10-26 00:06:57 +0200

    rpy2 migration, demonstrate the column parameter feature request

--- tools/correlation/cor.py
+++ tools/correlation/cor.py
@@ -6,7 +6,9 @@ usage: %prog infile output.txt columns method
 """
 
 import sys
-from rpy import *
+import rpy2.robjects as robjects
+r = robjects.r
+
 
 def stop_err(msg):
     sys.stderr.write(msg)
@@ -17,17 +19,25 @@ def main():
     assert method in ( "pearson", "kendall", "spearman" )
 
     try:
-        columns = map( int, sys.argv[3].split( ',' ) )
+        column_string = sys.argv[3]
+        columns = list()
+        for col in column_string.split(','):
+            if '-' in col:
+                s, e = col.split('-')
+                col = list(range(int(s), int(e) + 1))
+                columns.extend(col)
+            else:
+                columns.append(int(col))
     except:
         stop_err( "Problem determining columns, perhaps your query does not contain a column of numerical data." )
-    
+
     matrix = []
     skipped_lines = 0
     first_invalid_line = 0
     invalid_value = ''
     invalid_column = 0
 
-    for i, line in enumerate( file( sys.argv[1] ) ):
+    for i, line in enumerate( open( sys.argv[1] ) ):
         valid = True
         line = line.rstrip('\n\r')
 
@@ -60,29 +70,32 @@ def main():
                 first_invalid_line = i+1
 
         if valid:
-            matrix.append( row )
+            matrix += row 
 
     if skipped_lines < i:
-        try:
-            out = open( sys.argv[2], "w" )
-        except:
-            stop_err( "Unable to open output file" )
-
         # Run correlation
         try:
-            value = r.cor( array( matrix ), use="pairwise.complete.obs", method=method )
-        except Exception, exc:
-            out.close()
-            stop_err("%s" %str( exc ))
-        for row in value:
-            print >> out, "\t".join( map( str, row ) )
-        out.close()
+            fv = robjects.FloatVector(matrix)
+            m = r['matrix'](fv, ncol=len(columns),byrow=True)
+            rslt_mat = r.cor(m, use="pairwise.complete.obs", method=method )
+            value = []
+            for ri in range(1, rslt_mat.nrow + 1):
+                row = []
+                for ci in range(1, rslt_mat.ncol + 1):
+                    row.append(rslt_mat.rx(ri,ci)[0])
+                value.append(row)
+        except Exception as exc:
+            stop_err("%s" % str( exc ))
+        
+        with open( sys.argv[2], "w" ) as out:
+            for row in value:
+                out.write("%s\n" % "\t".join( map( str, row ) ))
 
     if skipped_lines > 0:
         msg = "..Skipped %d lines starting with line #%d. " %( skipped_lines, first_invalid_line )
         if invalid_value and invalid_column > 0:
             msg += "Value '%s' in column %d is not numeric." % ( invalid_value, invalid_column )
-        print msg
+        print(msg)
 
 if __name__ == "__main__":
     main()
