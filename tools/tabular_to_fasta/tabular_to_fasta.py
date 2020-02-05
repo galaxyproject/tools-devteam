22da7f3ca711ff04247a702aeabe63cb7a41b12c
Nicola Soranzo <nicola.soranzo@gmail.com>
2020-02-05 15:07:48 +0000

    Python code cleanup

--- tools/tabular_to_fasta/tabular_to_fasta.py
+++ tools/tabular_to_fasta/tabular_to_fasta.py
@@ -4,15 +4,12 @@ Input: fasta, minimal length, maximal length
 Output: fasta
 Return sequences whose lengths are within the range.
 """
-import sys
 import os
-
-assert sys.version_info[:2] >= (2, 4)
+import sys
 
 
 def stop_err(msg):
-    sys.stderr.write(msg)
-    sys.exit()
+    sys.exit(msg)
 
 
 def __main__():
@@ -29,41 +26,40 @@ def __main__():
         stop_err("Invalid Sequence Column: %s." % str(seq_col))
 
     title_col_list = title_col.split(',')
-    out = open(outfile, 'w')
     skipped_lines = 0
     first_invalid_line = 0
     invalid_line = ""
     i = 0
 
-    for i, line in enumerate(open(infile)):
-        error = False
-        line = line.rstrip('\r\n')
-        if line and not line.startswith('#'):
-            fields = line.split('\t')
-            fasta_title = []
-            for j in title_col_list:
-                try:
-                    j = int(j) - 1
-                    fasta_title.append(fields[j])
-                except:
-                    skipped_lines += 1
-                    if not invalid_line:
-                        first_invalid_line = i + 1
-                        invalid_line = line
-                    error = True
-                    break
-            if not error:
-                try:
-                    fasta_seq = fields[seq_col]
-                    if fasta_title[0].startswith(">"):
-                        fasta_title[0] = fasta_title[0][1:]
-                    print(">%s\n%s" % ("_".join(fasta_title), fasta_seq), file=out)
-                except:
-                    skipped_lines += 1
-                    if not invalid_line:
-                        first_invalid_line = i + 1
-                        invalid_line = line
-    out.close()
+    with open(outfile, 'w') as out:
+        for i, line in enumerate(open(infile)):
+            error = False
+            line = line.rstrip('\r\n')
+            if line and not line.startswith('#'):
+                fields = line.split('\t')
+                fasta_title = []
+                for j in title_col_list:
+                    try:
+                        j = int(j) - 1
+                        fasta_title.append(fields[j])
+                    except:
+                        skipped_lines += 1
+                        if not invalid_line:
+                            first_invalid_line = i + 1
+                            invalid_line = line
+                        error = True
+                        break
+                if not error:
+                    try:
+                        fasta_seq = fields[seq_col]
+                        if fasta_title[0].startswith(">"):
+                            fasta_title[0] = fasta_title[0][1:]
+                        print(">%s\n%s" % ("_".join(fasta_title), fasta_seq), file=out)
+                    except:
+                        skipped_lines += 1
+                        if not invalid_line:
+                            first_invalid_line = i + 1
+                            invalid_line = line
 
     if skipped_lines > 0:
         print('Data issue: skipped %d blank or invalid lines starting at #%d: "%s"' % (skipped_lines, first_invalid_line, invalid_line))
