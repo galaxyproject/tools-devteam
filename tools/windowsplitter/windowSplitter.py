2cdebda0021075bc64904ab59120ddf47e196e74
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2020-06-02 14:05:35 +0100

    Use with statement

--- tools/windowsplitter/windowSplitter.py
+++ tools/windowsplitter/windowSplitter.py
@@ -42,44 +42,41 @@ def main():
     except Exception:
         sys.exit("Data issue, click the pencil icon in the history item to correct the metadata attributes of the input dataset.")
 
-    fo = open(out_file, 'w')
-
     skipped_lines = 0
     first_invalid_line = 0
     invalid_line = None
     if offset == 0:
         makesliding = 0
 
-    for i, line in enumerate(open(inp_file)):
-        line = line.strip()
-        if line and line[0:1] != "#":
-            try:
-                elems = line.split('\t')
-                start = int(elems[start_col_1])
-                end = int(elems[end_col_1])
-                if makesliding == 0:
-                    numwin = (end - start) // winsize
-                else:
-                    numwin = (end - start) // offset
-                if numwin > 0:
-                    for _ in range(numwin):
-                        elems_1 = elems
-                        elems_1[start_col_1] = str(start)
-                        elems_1[end_col_1] = str(start + winsize)
-                        fo.write("%s\n" % '\t'.join(elems_1))
-                        if makesliding == 0:
-                            start = start + winsize
-                        else:
-                            start = start + offset
-                            if start + winsize > end:
-                                break
-            except Exception:
-                skipped_lines += 1
-                if not invalid_line:
-                    first_invalid_line = i + 1
-                    invalid_line = line
-
-    fo.close()
+    with open(out_file, 'w') as fo, open(inp_file) as fi:
+        for i, line in enumerate(fi):
+            line = line.strip()
+            if line and line[0:1] != "#":
+                try:
+                    elems = line.split('\t')
+                    start = int(elems[start_col_1])
+                    end = int(elems[end_col_1])
+                    if makesliding == 0:
+                        numwin = (end - start) // winsize
+                    else:
+                        numwin = (end - start) // offset
+                    if numwin > 0:
+                        for _ in range(numwin):
+                            elems_1 = elems
+                            elems_1[start_col_1] = str(start)
+                            elems_1[end_col_1] = str(start + winsize)
+                            fo.write("%s\n" % '\t'.join(elems_1))
+                            if makesliding == 0:
+                                start = start + winsize
+                            else:
+                                start = start + offset
+                                if start + winsize > end:
+                                    break
+                except Exception:
+                    skipped_lines += 1
+                    if not invalid_line:
+                        first_invalid_line = i + 1
+                        invalid_line = line
 
     if makesliding == 1:
         print('Window size=%d, Sliding=Yes, Offset=%d' % (winsize, offset))
